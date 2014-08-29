4c841c49e1e2445c6f6fc3501b6312441b53c2e3
Fabio Maggio <fabio@infoweb.be>
2014-08-29 11:10:19 +0200

    Register cms asset bundle

--- views/page-partial/create.php
+++ views/page-partial/create.php
@@ -1,7 +1,9 @@
 <?php
-
 use yii\helpers\Html;
+use infoweb\cms\CMSAsset;
 
+// Register assets
+CMSAsset::register($this);
 
 /* @var $this yii\web\View */
 /* @var $model infoweb\partials\models\PagePartial */
